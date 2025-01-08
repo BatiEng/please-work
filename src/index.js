c819d283849b58dbfd00037cc8b903809c917289
Ali Batikan Ocal <alibatikan@gmail.com>
2025-01-08 15:05:41 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const RSSParser = require("rss-parser");
 
 const app = express();
 app.use(cors());
 
+const rssUrl = "https://www.ntv.com.tr/son-dakika.rss";
+
+const parser = new RSSParser();
+let articles = [];
+
+const parse = async (url) => {
+  let counter = 1;
+  let feed = null;
+  while (feed === null) {
+    counter += 1;
+    feed = await parser.parseURL(url);
+    if (counter > 9) {
+      break;
+    }
+  }
+  feed.items.forEach((item) => {
+    articles.push(item);
+  });
+};
+parse(rssUrl);
+
 app.get("/", (req, res) => {
-  res.send("batikan ocal");
+  res.send(articles);
 });
 
 app.listen(3000, () => {
