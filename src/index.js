860b32975ee7314a470178c0eca7c8bf98e4ce82
Ali Batikan Ocal <alibatikan@gmail.com>
2025-01-08 14:55:35 +0300

    new project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.send("batikan ocal");
+});
+
+app.listen(3000, () => {
+  console.log("app is listening via port 3000");
+});
